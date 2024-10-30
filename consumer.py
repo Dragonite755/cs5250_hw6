935607eb30090e0187e3e2ea67b9f700ad1014a0
Bryan Armenta <a02300296@usu.edu>
2024-10-30 14:12:31 -0600

    Add methods to create request and source objects from args

--- consumer.py
+++ consumer.py
@@ -1,8 +1,10 @@
-import boto3
 import time
 import json
 import jsonschema
 import argparse
+
+import widget_source
+import widget_destination
 
 REQUESTS_BUCKET = "usu-cs5250-ratatouille-requests"
 JSON_SCHEMA = {
@@ -61,9 +63,31 @@ JSON_SCHEMA = {
 class Consumer:
     def __init__(self, source, destination):
         self.__source = source
-        self.__destination = destination
+        self.__destination = destination 
         
-    
+    def process_requests(self, timeout=5):
+        end_time = time.time() + timeout
+        while self.check_time(end_time):
+            # Retrieve request as JSON dictionary
+            request = self.__source.poll_request()
+            if not self.process_request(request):
+                time.sleep(0.1)
+        
+    def process_request(self, request): 
+        if not request:
+            return False
+        elif request["type"] == "create":
+            self.__destination.create(request)
+        elif request["type"] == "delete":
+            self.__destination.delete(request)
+        elif request["type"] == "update":
+            self.__destination.update(request)
+        else:
+            return False
+        return True
+        
+    def check_time(self, end_time):
+        return time.time() < end_time
     
 def create_command_parser():
     parser = argparse.ArgumentParser(
@@ -76,9 +100,23 @@ def create_command_parser():
     group.add_argument("-wb", "--widget-bucket", metavar="Widget Bucket") # Bucket
     group.add_argument("-dwt", metavar="Widget DynamoDB Table") # DynamoDB table
     return parser
+    
+def parse_source(args):
+    if args.request_bucket:
+        return widget_source.BucketSource(args.request_bucket)
         
+def parse_destination(args):
+    if args.widget_bucket:
+        return widget_destination.BucketDestination(args.widget_bucket)
+
 if __name__ == "__main__":
     command_parser = create_command_parser()
     args = command_parser.parse_args()
     print(args)
-    print(args.request_bucket)
+    print()
+    
+    source = parse_source(args)
+    destination = parse_destination(args)
+    consumer = Consumer(source, destination)
+    
+    consumer.process_requests()
